991f68e94c0f98ed384654de4284305f910f0bd0
mariopino <info@quequiereshacer.es>
2020-06-22 12:42:47 +0200

    fix typo

--- autopayout.js
+++ autopayout.js
@@ -126,7 +126,7 @@ const main = async () => {
         if (lastClaimedReward < currentEra) {
           console.log(`\x1b[1m -> ${currentEra - lastClaimedReward - 2} unclaimed era rewards\x1b[0m`);
           let transactions = [];
-          for (let era = lastClaimedReward + 1; era > currentEra; era++) {
+          for (let era = lastClaimedReward + 1; era < currentEra; era++) {
             transactions.push(api.tx.staking.payoutStakers(validator, era));
           }
 
